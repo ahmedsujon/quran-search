006425a52a1ebb651f8a968b4f80f591ffb922af
ahmedsujon <sujonahmed424@gmail.com>
2022-09-09 17:01:17 +0600

    intro text

--- resources/views/livewire/app/index-component.blade.php
+++ resources/views/livewire/app/index-component.blade.php
@@ -1,10 +1,25 @@
 <div>
     <div class="px-4 py-5 my-5 text-center">
-        <h1 style="padding-top: 100px;" class="display-5 fw-bold">Quran Database Search</h1>
+        <h1 class="display-5 fw-bold">Quran Database Search</h1>
         <div class="col-lg-6 mx-auto">
-            <p class="lead mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium qui a, consectetur
-                necessitatibus dolores cumque saepe quaerat totam, minus porro velit, beatae modi cum nobis eum. Earum
-                similique dolor veniam.</p>
+            <div class="intro-text text-right">
+                <ul class="text-start">
+                    <li class="mb-2">Provides Both English and Arabic Search with Single Word or Multiple Word Search with "OR" and "AND" Capability.</li>
+                    <li class="mb-2">In Arabic the search can either be done "Root", "Actual", or "Normalize" Word.</li>
+                    <li class="mb-2">English Search can be by Word/Subject/Category.</li>
+
+                    <li class="mb-2">English Search provides capability of Inferring words, i.e, it will display verse 2.2 when you search for the word "Quran" even though verse 2.2 does not have the word Quran. Similarly, it will infer and display words or subjects that are directly mentioned in the verse but are connected and related to words or subjects discussed in the previous verse.</li>
+
+                    <li class="mb-2">New Words/Sujects/Categories have been grouped together for easy search, i.e. Verses addressed to Belivers, Prophets, Disbelivers, etc. Similarly verses were grouped togher to show "Conversation Between Allah & Angels", "Conversation Between Inhabitants of Paradies", etc. Also other verses like "No Fear or Grief", "Whom Allah Guides", "Whom Allah Loves", "How to Receive Mercy of Allah", etc. were grouped together for easy search.</li>
+
+                    <li class="mb-2">Also, a category was created to reflect where two same words appear repeatedly in the Quran, i.e., "Verse on Heaven & Earth", "Verse on Prayer & Zakat", "Verse on Allah $ His Messanger", etc.</li>
+
+                    <li class="mb-2">Also a category was created to reflect the theme of a verse, or a series of verses, i.e., Inheritance, Divorce, Nightly Worship, etc.</li>
+
+                    <li class="mb-2">Also, a category for grouping the verses which pertain to some, i.e, "Story of Moses", "Story of Adam", "Story of Noah", "Story of the two gardens in surah Kahf", etc. were created.</li>
+                    <li>A sample list of Selected search was also included, and this list can be expanded to include new searches in the future.</li>
+                </ul>
+            </div>
             <div class="d-grid gap-3 d-sm-flex justify-content-sm-center">
                 <a style="padding: 20px; width: 300px;" href="{{ route('search-list') }}"
                     class="btn btn-primary btn-lg px-4 gap-3">Beginning Search</a>
